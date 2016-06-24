97d14169883ec6c0401d8d9db3c18ffbdb34e13c
Mike Klein <mtklein@google.com>
2016-06-24 17:16:56 -0400

    simplify wiring

--- test.c
+++ test.c
@@ -6,10 +6,7 @@ static int dst[1024], src[1024];
 static char cov[1024];
 
 int main(int argc, char** argv) {
-    int n = 1;
-    if (argc > 1) {
-        n = atoi(argv[1]);
-    }
+    int n = argc > 1 ? atoi(argv[1]) : 1;
 
     for (int j = 0; j < n; j++) {
         fused(dst, src, cov, 1024);
@@ -17,19 +14,17 @@ int main(int argc, char** argv) {
 
     for (int j = 0; j < n; j++) {
         struct stage stages[] = {
-            { NULL,         load_d_srgb, NULL },
-            { NULL,         load_s_srgb,  src },
-            { NULL,             srcover, NULL },
-            { NULL,             lerp_u8,  cov },
-            { NULL, store_s_load_d_srgb, NULL },
+            { (void*)1,         load_d_srgb, NULL },
+            { (void*)2,         load_s_srgb,  src },
+            { (void*)3,             srcover, NULL },
+            { (void*)4,             lerp_u8,  cov },
+            { (void*)1, store_s_load_d_srgb, NULL },
         };
 
-        size_t nstages = sizeof(stages) / sizeof(*stages);
-
-        for (size_t i = 0; i < nstages-1; i++) {
-            stages[i].next = &stages[i+1];
+        for (size_t i = 0; i < sizeof(stages) / sizeof(*stages); i++) {
+            size_t next = (size_t)stages[i].next;
+            stages[i].next = &stages[next];
         }
-        stages[4].next = &stages[1];
 
         stages[0].fn(&stages[0], 1023, dst, _mm_setzero_ps(), _mm_setzero_ps());
     }
