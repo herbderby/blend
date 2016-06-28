ec25a1df9d77bfc5e622dd03552214a96c3b0845
Mike Klein <mtklein@chromium.org>
2016-06-28 13:16:46 -0400

    rename -> stage_fn

--- pipeline.h
+++ pipeline.h
@@ -4,20 +4,20 @@
 
 struct stage;
 
-typedef void blend(struct stage*, size_t n, void* dp, __m128 d, __m128 s);
+typedef void stage_fn(struct stage*, size_t n, void* dp, __m128 d, __m128 s);
 
 struct stage {
     struct stage* next;
-    blend* fn;
+    stage_fn* fn;
     const void* const_ctx;
     void* ctx;
 };
 
-blend splice_next,
-      done_yet,
-      load_d_srgb,
-      load_s_srgb,
-      srcover,
-      lerp_u8,
-      store_s_srgb,
-      store_s_done_yet_load_d_srgb;
+stage_fn splice_next,
+         done_yet,
+         load_d_srgb,
+         load_s_srgb,
+         srcover,
+         lerp_u8,
+         store_s_srgb,
+         store_s_done_yet_load_d_srgb;
