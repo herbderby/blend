151185e5209936eb99206c5a91c3bbd20bb0a358
Mike Klein <mtklein@google.com>
2016-07-05 18:27:48 -0400

    slow and fast src

--- test.cc
+++ test.cc
@@ -33,6 +33,19 @@ static void src_mask(uint32_t* dp, const uint32_t* sp, const uint8_t* cp, size_t
     p.call(n);
 }
 
+static void src_slow(uint32_t* dp, const uint32_t* sp, size_t n) {
+    pipeline p;
+    p.add_stage(pipeline::   load_srgb,      sp, nullptr);
+    p.add_stage(pipeline::  store_srgb, nullptr,      dp);
+    p.ready();
+
+    p.call(n);
+}
+
+static void src_fast(uint32_t* dp, const uint32_t* sp, size_t n) {
+    memcpy(dp, sp, n*sizeof(uint32_t));
+}
+
 static uint32_t dst[1023], src[1023];
 static uint8_t cov[1023];
 
@@ -43,12 +56,16 @@ int main(int argc, char** argv) {
 
     for (int j = 0; j < 100000; j++) {
         switch(choice) {
-            case 1: srcover     (dst, src,      1023); break;
-            case 2: srcover_mask(dst, src, cov, 1023); break;
-            case 3:     src_mask(dst, src, cov, 1023); break;
+            case 1:     src_fast(dst, src,      1023); break;
+            case 2:     src_slow(dst, src,      1023); break;
+            case 3: srcover     (dst, src,      1023); break;
+            case 4: srcover_mask(dst, src, cov, 1023); break;
+            case 5:     src_mask(dst, src, cov, 1023); break;
         }
     }
 
+        src_slow(dst, src,      1023);
+        src_fast(dst, src,      1023);
     srcover     (dst, src,      1023);
     srcover_mask(dst, src, cov, 1023);
         src_mask(dst, src, cov, 1023);
